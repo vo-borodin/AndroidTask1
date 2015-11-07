66b401de64d36bb33b17b29918425df494cc4900
vo-borodin <brdnvsvd89@mail.ru>
2015-11-07 14:52:17 +0600

    Добавление второй и третьей Activity, установка на первой Activity кнопок, ведущих на другие две Activity

--- app/src/main/java/com/example/voborodin/androidtask1/FirstActivity.java
+++ app/src/main/java/com/example/voborodin/androidtask1/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.voborodin.androidtask1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void goToSecondActivity(View view)
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToThirdActivity(View view)
+    {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
